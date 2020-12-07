43b4061f796485a8691fa041119348a023994237
georg3tom <georg3tom@gmail.com>
2020-12-07 22:14:24 +0530

    route update

--- webapp/app/route.py
+++ webapp/app/route.py
@@ -4,11 +4,21 @@ from os.path import join, dirname, realpath
 from app import app
 from flask import render_template, request, jsonify
 import numpy as np
+import pickle
+from PIL import Image
+import io
+import cv2
+import numpy as np
 
 sys.path.insert(
     0,
-    realpath(os.path.join(dirname(realpath(__file__)), "/../")),
+    realpath(os.path.join(dirname(realpath(__file__)), "../../")),
 )
+
+from image_retrieval.main import ImageRetriever
+
+ir = ImageRetriever()
+ir.load_hash_table(filename='../hash_table.pickle')
 
 @app.route("/")
 def root():
@@ -26,7 +36,13 @@ def searchUpload():
     f = request.files["file-0"]
 
     img = Image.open(io.BytesIO(f.stream.read()))
-    img = cv2.cvtColor(np.float32(img), cv2.COLOR_RGB2GRAY)
+    img = np.array(img).astype(np.uint8)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    ret = ir.query(img)
+    images = []
+    votes = []
+    for i in ret:
+        images.append(i[1])
 
     if img is None:
         print("Error, couldn't open file")
